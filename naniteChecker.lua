367ef6907604b17a4625ddc637bbc36853c51f34
Good <71841069+jahvai@users.noreply.github.com>
2024-07-15 17:52:56 +0000

    Jul 15, 2024, 1:52 PM

--- naniteChecker.lua
+++ naniteChecker.lua
@@ -12,7 +12,7 @@ _, _, _, _, _, _, _, inputs = event.pull("modem_message")
 print(inputs)
 for i = 1, inputs do
 m.broadcast(1,"nanomachines", "setInput", i, true)
-os.sleep(1)
+os.sleep(0.75)
 m.broadcast(1,"nanomachines", "getActiveEffects")
 _, _, _, _, _, _, _, result = event.pull("modem_message")
 print(i, result)
