7a977f2b7954a819c9b2f7bf94c958a7bb61b28f
Good <71841069+jahvai@users.noreply.github.com>
2024-07-15 01:00:51 +0000

    Jul 14, 2024, 9:00 PM

--- naniteChecker.lua
+++ naniteChecker.lua
@@ -5,8 +5,10 @@ event = require("event")
 modem.open(2)
 modem.broadcast(1,"nanomachines", "setResponsePort", 2)
 _, _, _, _, _, name = event.pull(10,"modem_message")
-if name ~= nil then
-for i = 1, modem.broadcast(1,"nanomachines", "getTotalInputCount") + 1 do
+if name == "SkiddyMango3125" then
+modem.broadcast(1,"nanomachines", "getTotalInputCount")
+inputs = event.pull(10, "modem_message")
+for i = 1, inputs + 1 do
 modem.broadcast(1,"nanomachines", "setInput", i, true)
 os.sleep(1)
 modem.broadcast(1,"nanomachines", "getActiveEffects")
