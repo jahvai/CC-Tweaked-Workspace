c9a50297f3d252309ecbd5f900e83af4fec52355
Good <71841069+jahvai@users.noreply.github.com>
2024-07-15 01:58:53 +0000

    Jul 14, 2024, 9:58 PM

--- naniteChecker.lua
+++ naniteChecker.lua
@@ -6,19 +6,19 @@ m.open(2)
 m.open(1)
 
 m.broadcast(1,"nanomachines", "setResponsePort", 2)
-m.broadcast(1, "nanomachines", "getName")
-_, _, _, _, _, _, _, name = event.pull("modem_message")
-print(name)
-if name ~= "" then
+event.pull("modem_message")
 m.broadcast(1,"nanomachines", "getTotalInputCount")
 _, _, _, _, _, _, _, inputs = event.pull("modem_message")
 print(inputs)
 for i = 1, inputs + 1 do
 m.broadcast(1,"nanomachines", "setInput", i, true)
+event.pull("modem_message")
 os.sleep(1)
 m.broadcast(1,"nanomachines", "getActiveEffects")
 _, _, _, _, _, _, _, result = event.pull("modem_message")
 print(result)
-m.broadcast(1,"nanomachines", "setInput", i, false)
 end
+
+for i = 1, inputs + 1 do
+m.broadcast(1,"nanomachines", "setInput", i, false)
 end
