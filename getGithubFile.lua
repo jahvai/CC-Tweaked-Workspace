ffa198d4d95e1efdd9f8dc8ba39b54148e96ad53
Good <71841069+jahvai@users.noreply.github.com>
2025-07-14 12:50:00 -0500

    Update getGithubFile.lua

--- getGithubFile.lua
+++ getGithubFile.lua
@@ -2,5 +2,6 @@ if arg[1] == nil then
 print("Name of File To Retrieve:")
 arg[1] = read()
 end
+
 file = arg[1]
 shell.run("wget", "https://raw.githubusercontent.com/jahvai/CC-Tweaked-Workspace/refs/heads/main/"..file, file)
