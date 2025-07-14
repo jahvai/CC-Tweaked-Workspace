5bc5b24ca58e4b38bf45678a4e56641a8db029f0
Good <71841069+jahvai@users.noreply.github.com>
2025-07-14 12:49:26 -0500

    Update utilityApi.lua

--- utilityApi.lua
+++ utilityApi.lua
@@ -2,10 +2,10 @@ local function isInBetween(base, num1, num2)
 return (base >= num1 and base <= num2) or (base <= num1 and base >= num2)
 end
 
-local function clear ()
+local function clear()
 term.setCursorPos(0, 0)
 shell.run("clear")
 end
 
 
-return { isInBetween = isInBetween, }
+return { isInBetween = isInBetween, clear = clear}
