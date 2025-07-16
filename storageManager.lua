20e8a7094353c88f456821314b7ea53447fab519
Good <71841069+jahvai@users.noreply.github.com>
2025-07-15 22:52:26 -0500

    Cleaned up an unneccessary print statement

--- storageManager.lua
+++ storageManager.lua
@@ -7,7 +7,6 @@ peripheral.find("inventory")
 local totals = {}
 for i, s in ipairs(storages) do
 for k, v in pairs(s.list()) do
-print(v.count,"aa")
 totals = util.runningTotal(totals, v.name, v.count)
 end
 end
