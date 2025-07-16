5d6d979f9cb1996b4dff37b37980b91ecbfd1d22
Good <71841069+jahvai@users.noreply.github.com>
2025-07-15 20:27:28 -0500

    Added the ability to force add a file from github; the ability to update a file.

--- getGithubFile.lua
+++ getGithubFile.lua
@@ -1,16 +1,20 @@
 if arg[1] == nil then
-write("Name of File To Retrieve: ")
-arg[1] = read()
+    write("Name of File To Retrieve: ")
+    arg[1] = read()
 end
 
 if arg[2] == nil then
-arg[2] = arg[1]
+    arg[2] = arg[1]
 end
 
 local file = arg[1]
 local filename = arg[2]
+if (arg[3] == "force") or (arg[3] == "f") then
+    shell.run("rm "..filename)
+end
+
 if string.lower(arg[2]) ~= "run" then
-shell.run("wget", "https://raw.githubusercontent.com/jahvai/CC-Tweaked-Workspace/refs/heads/main/"..file, filename)
+    shell.run("wget", "https://raw.githubusercontent.com/jahvai/CC-Tweaked-Workspace/refs/heads/main/"..file, filename)
 else
-shell.run("wget", "run", "https://raw.githubusercontent.com/jahvai/CC-Tweaked-Workspace/refs/heads/main/"..file)
+    shell.run("wget", "run", "https://raw.githubusercontent.com/jahvai/CC-Tweaked-Workspace/refs/heads/main/"..file)
 end
