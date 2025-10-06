5cdd07a7f80e30add15d2fd6db329ed7bf43f25d
Good <71841069+jahvai@users.noreply.github.com>
2025-10-05 22:35:24 -0400

    Added functionality to requestItem() to allow lists of output inventories and items names.

--- storageApi.lua
+++ storageApi.lua
@@ -26,17 +26,32 @@ end
 
 -- Reset item list after adding getting.
 local function requestItem(outputInventory, itemName, itemList)
-    if type(outputInventory) == "string" then
-        outputInventory = peripheral.wrap(outputInventory)
+    if type(outputInventory) ~= "table" then
+        outputInventory = {outputInventory}
     end
-    for i, v in ipairs(itemList) do
-        if (v[1].name == itemName) and (v[2] ~= peripheral.getName(outputInventory)) then
-            outputInventory.pullItems(v[2], v[3])
-            itemList = table.remove(itemList, i)
-            return true, itemList
+    if type(itemName) ~= "table" then
+        itemName = {itemName}
+    end
+
+    for i, v in ipairs(outputInventory) do
+        if type(v) == "string" then
+            outputInventory[i] = peripheral.wrap(v)
+        end
+    end
+
+    for _, outputInventoryPeripheral in ipairs(outputInventory) do 
+        for itemIndex, itemData in ipairs(itemList) do
+            for _, currentItem in ipairs(itemName) do
+                if (itemData[1].name == currentItem) and (itemData[2] ~= peripheral.getName(outputInventoryPeripheral)) then
+                    if not outputInventoryPeripheral.pullItems(itemData[2], itemData[3]) then goto continueLoop end
+                    itemList = table.remove(itemList, itemIndex)
+                    return true, itemList
+                    ::continueLoop::
+                end
+            end
         end
     end
     return false, itemList
 end
 
-return { findInventories = findInventories, getAllItems = getAllItems, requestItem = requestItem}
+return { findInventories = findInventories, getAllItems = getAllItems, requestItem = requestItem }
