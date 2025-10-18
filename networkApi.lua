5610d1d73bb4b8c01e452e096d242dc6e336d69f
Good <71841069+jahvai@users.noreply.github.com>
2025-10-18 16:51:24 -0400

    Added some functionality

--- networkApi.lua
+++ networkApi.lua
@@ -1,16 +1,40 @@
 local modem = peripheral.find("modem")
 
+-- Add futher functionality later
+local function setUpServer(serverCode)
+    return {code = serverCode}
+end
 
-local function getResponse(port, data)
-    modem.transmit(port,port,data)
-    if not modem.isOpen(port) then
+local function createCallback(name, func)
+    return {name, func}
+-- {code, {function: str, params: table}}
+-- function(table.unpack(table))
+end
+
+local function runCallbacks()
+
+local function getResponse(port, code, data)
+    portWasOpen = modem.isOpen(port)
+    if not portWasOpen then
         modem.open(port)
     end
+
+    modem.transmit(port, port, {code, data})
+
+    local networkMessage
+    repeat
+        _, _, _, _, networkMessage, _ = os.pullEvent("modem_message")
+    until networkMessage[1] = code
+
+    if not portWasOpen then
+        modem.close(port)
+    end
+    return table.unpack(networkMessage)
 end
 
 
-local networkApiTable = {}
+local networkApiTable = {modem = modem, getResponse = getResponse}
 networkApiTable["client"] = {}
-networkApiTable["server"] = {}
+networkApiTable["server"] = {setUpServer = setUpServer}
 
 return networkApiTable
